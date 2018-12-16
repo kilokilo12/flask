89c148b2664f5176162915422f04f654c92d5077
Kaire Kimmel <kaire_kimmel@yahoo.co.uk>
2018-12-16 10:13:52 +0100

    Added a functioning contact form

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "some_secret"
 
 
 @app.route("/about/<member_name>")
@@ -32,8 +33,11 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form["name"]))
     return render_template("contact.html", page_title="Contact")
 
 
